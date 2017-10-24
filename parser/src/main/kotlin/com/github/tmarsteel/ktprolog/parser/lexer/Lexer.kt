2d2ff4d1878d4b9ee074bcf0172a241557ba3b91
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-10-24 18:29:53 +0200

    Parser: Implemented lexer with basic support
    Still TODO:
    Quoted Atoms (i.e. Strings),
    comments

--- parser/src/main/kotlin/com/github/tmarsteel/ktprolog/parser/lexer/Lexer.kt
+++ parser/src/main/kotlin/com/github/tmarsteel/ktprolog/parser/lexer/Lexer.kt
@@ -1,10 +1,216 @@
 package com.github.tmarsteel.ktprolog.parser.lexer
 
-import com.github.tmarsteel.ktprolog.parser.sequence.Index
+import com.github.tmarsteel.ktprolog.parser.sequence.IteratorBasedTransactionalSequence
 import com.github.tmarsteel.ktprolog.parser.sequence.TransactionalSequence
 import com.github.tmarsteel.ktprolog.parser.source.SourceLocation
+import com.github.tmarsteel.ktprolog.parser.source.SourceLocationRange
+import com.github.tmarsteel.ktprolog.parser.source.SourceUnit
 
-interface Lexer {
-    fun lex(input: TransactionalSequence<Char, SourceLocation>): TransactionalSequence<Token, Index>
+class Lexer(unit: SourceUnit, source: Iterator<Char>) : TransactionalSequence<Token> {
+    private val lexerIteratorTxSequence = IteratorBasedTransactionalSequence(LexerIterator(unit, source))
+
+    override fun mark() = lexerIteratorTxSequence.mark()
+
+    override fun commit() = lexerIteratorTxSequence.commit()
+
+    override fun hasNext() = lexerIteratorTxSequence.hasNext()
+
+    override fun next() = lexerIteratorTxSequence.next()
+
+    override fun rollback() = lexerIteratorTxSequence.rollback()
 }
 
+internal class LexerIterator(unit: SourceUnit, givenSource: Iterator<Char>) : Iterator<Token> {
+
+    private val source: TransactionalSequence<Pair<Char, SourceLocation>>
+    init {
+        source = IteratorBasedTransactionalSequence(
+                SourceLocationAwareCharIterator(
+                        initial = SourceLocation(unit, 1, 1),
+                        source = givenSource
+                )
+        )
+    }
+
+    private var next: Token? = findNext()
+
+    override fun hasNext() = next != null
+
+    override fun next(): Token {
+        if (next == null) throw NoSuchElementException()
+
+        val token = next!!
+        next = findNext()
+        return token
+    }
+
+    /**
+     * @return The nextToken token from the input or `null` if there are no more tokens.
+     */
+    private fun findNext(): Token? {
+        skipWhitespace()
+
+        if (!source.hasNext()) return null
+
+        // try to match an operator
+        val operatorToken = tryMatchOperator()
+        if (operatorToken != null) return operatorToken
+
+        if (!source.hasNext()) return null
+
+        var text = collectUntilOperatorOrWhitespace()
+
+        if (text.first[0].isDigit()) {
+            // NUMERIC LITERAL
+            val numberWithLocation: Pair<Number, SourceLocationRange>
+            source.mark()
+
+            var next = source.next()
+            if (next.first == DECIMAL_SEPARATOR) {
+                next = source.next()
+
+                if (next.first.isDigit()) {
+                    // <DIGIT...> <SEPARATOR> <DIGIT...> => floating point literal
+                    source.commit()
+                    val afterSeparator = collectUntilOperatorOrWhitespace()
+                    val numberLocationRange = SourceLocationRange(text.second.start, afterSeparator.second)
+                    val numberAsString = text.first + '.' + afterSeparator.first
+                    numberWithLocation = Pair(numberAsString.toDouble(), numberLocationRange)
+                }
+                else {
+                    // <DIGIT...> <SEPARATOR> <!DIGIT> => treat the separator as an operator; the nextCharsAsString invocation will
+                    // find it and deal with it
+                    source.rollback()
+                    numberWithLocation = Pair(text.first.toLong(), text.second)
+                }
+            }
+            else {
+                numberWithLocation = Pair(text.first.toLong(), text.second)
+            }
+
+            return NumericLiteralToken(numberWithLocation.first, numberWithLocation.second)
+        }
+        else {
+            // IDENTIFIER
+            return IdentifierToken(text.first, text.second)
+        }
+    }
+
+    private fun nextCharsAsString(n: Int): Pair<String, SourceLocationRange>? {
+        if (n < 1) throw IllegalArgumentException()
+
+        var start: SourceLocation? = null
+        var end: SourceLocation? = null
+        val charAr = CharArray(n)
+        for (i in 0 until n) {
+            if (source.hasNext()) {
+                val next = source.next()
+                charAr[i] = next.first
+
+                if (start == null) {
+                    start = next.second
+                }
+
+                end = next.second
+            }
+            else {
+                return null
+            }
+        }
+
+        return Pair(String(charAr), SourceLocationRange(start!!, end!!))
+    }
+
+    private fun tryMatchOperator(doCommit: Boolean = true): OperatorToken? {
+        for (operator in Operator.values())
+        {
+            source.mark()
+
+            val nextText = nextCharsAsString(operator.text.length)
+            if (nextText != null && nextText.first == operator.text) {
+                if (doCommit) source.commit() else source.rollback()
+                return OperatorToken(operator, nextText.second)
+            }
+
+            source.rollback()
+        }
+
+        return null
+    }
+
+    private fun collectUntilOperatorOrWhitespace(): Pair<String, SourceLocationRange> {
+        val buf = StringBuilder()
+        var start: SourceLocation? = null
+        var end: SourceLocation? = null
+
+        while (source.hasNext()) {
+            val operator = tryMatchOperator(false)
+            if (operator != null) break
+
+            source.mark()
+            val next = source.next()
+
+            if (IsWhitespace(next.first)) {
+                source.rollback()
+                break
+            }
+
+            if (start == null) {
+                start = next.second
+            }
+            end = next.second
+
+            source.commit()
+
+            buf.append(next.first)
+        }
+
+        return Pair(buf.toString(), SourceLocationRange(start!!, end!!))
+    }
+
+    /**
+     * Collects chars from the input as long as they fulfill [pred]. If a character is encountered that does not
+     * fulfill the predicate, the collecting is stopped and the characters collected so far are returned. The character
+     * causing the break can then be obtained using [nextCharsAsString].
+     */
+    private fun collectWhile(pred: (Char) -> Boolean): Pair<String, SourceLocationRange>?
+    {
+        var buf = StringBuilder()
+        var start: SourceLocation? = null
+        var end: SourceLocation? = null
+
+        var charWithlocation: Pair<Char, SourceLocation>
+        while (source.hasNext())
+        {
+            source.mark()
+            charWithlocation = source.next()
+
+            if (start == null) {
+                start = charWithlocation.second
+            }
+
+            if (pred(charWithlocation.first)) {
+                buf.append(charWithlocation.first)
+                end = charWithlocation.second
+                source.commit()
+            }
+            else {
+                source.rollback()
+                break
+            }
+        }
+
+        if (start != null && end != null) {
+            return Pair(buf.toString(), SourceLocationRange(start, end))
+        }
+        else {
+            return null
+        }
+    }
+
+    private fun skipWhitespace() {
+        collectWhile(IsWhitespace)
+    }
+}
+
+private val IsWhitespace: (Char) -> Boolean = { c -> c == ' ' || c == '\t' || c == '\n' || c == '\r' }
---
+++ parser/src/test/com/github/tmarsteel/ktprolog/parser/lexer/LexerIteratorTest.kt
@@ -0,0 +1,254 @@
+package com.github.tmarsteel.ktprolog.parser.lexer
+
+import com.github.tmarsteel.ktprolog.parser.source.SourceUnit
+import io.kotlintest.specs.FreeSpec
+
+class LexerIteratorTest : FreeSpec() {init{
+    "test" {
+        val source = """predicate(arg).
+            predicate(arg1, arg2).
+            ruleHeadPredicate(arg1, X) :- goal1(arg1), goal2(X).
+        """
+        val lexer = LexerIterator(SourceUnit("testcode"), source.asIterable().iterator())
+
+        var next: Token
+
+        "line 1" - {
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "predicate"
+            next.location.start.line shouldEqual 1
+            next.location.start.column shouldEqual 1
+            next.location.end.line shouldEqual 1
+            next.location.end.column shouldEqual 9
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_OPEN
+            next.location.start.line shouldEqual 1
+            next.location.start.column shouldEqual 10
+            next.location.end.line shouldEqual 1
+            next.location.end.column shouldEqual 10
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "arg"
+            next.location.start.line shouldEqual 1
+            next.location.start.column shouldEqual 11
+            next.location.end.line shouldEqual 1
+            next.location.end.column shouldEqual 13
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_CLOSE
+            next.location.start.line shouldEqual 1
+            next.location.start.column shouldEqual 14
+            next.location.end.line shouldEqual 1
+            next.location.end.column shouldEqual 14
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.FULL_STOP
+            next.location.start.line shouldEqual 1
+            next.location.start.column shouldEqual 15
+            next.location.end.line shouldEqual 1
+            next.location.end.column shouldEqual 15
+        }
+
+        "line 2" - {
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "predicate"
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 13
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 21
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_OPEN
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 22
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 22
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "arg1"
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 23
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 26
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.COMMA
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 27
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 27
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "arg2"
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 29
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 32
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_CLOSE
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 33
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 33
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.FULL_STOP
+            next.location.start.line shouldEqual 2
+            next.location.start.column shouldEqual 34
+            next.location.end.line shouldEqual 2
+            next.location.end.column shouldEqual 34
+        }
+
+        "line 3" - {
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "ruleHeadPredicate"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 13
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 29
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_OPEN
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 30
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 30
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "arg1"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 31
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 34
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.COMMA
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 35
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 35
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "X"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 37
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 37
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_CLOSE
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 38
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 38
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.HEAD_QUERY_SEPARATOR
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 40
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 41
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "goal1"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 43
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 47
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_OPEN
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 48
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 48
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "arg1"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 49
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 52
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_CLOSE
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 53
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 53
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.COMMA
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 54
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 54
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "goal2"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 56
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 60
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_OPEN
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 61
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 61
+
+            next = lexer.next()
+            assert(next is IdentifierToken)
+            (next as IdentifierToken).textContent shouldEqual "X"
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 62
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 62
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.PARANT_CLOSE
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 63
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 63
+
+            next = lexer.next()
+            assert(next is OperatorToken)
+            (next as OperatorToken).operator shouldEqual Operator.FULL_STOP
+            next.location.start.line shouldEqual 3
+            next.location.start.column shouldEqual 64
+            next.location.end.line shouldEqual 3
+            next.location.end.column shouldEqual 64
+        }
+    }
+}}
