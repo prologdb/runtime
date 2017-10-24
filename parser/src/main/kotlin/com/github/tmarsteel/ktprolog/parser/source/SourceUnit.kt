a63635f39ff8cd51e014f4d040a2358aa7b72747
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-10-24 11:08:05 +0200

    Parser: added code to model source locations

---
+++ parser/src/main/kotlin/com/github/tmarsteel/ktprolog/parser/source/SourceUnit.kt
@@ -0,0 +1,6 @@
+package com.github.tmarsteel.ktprolog.parser.source
+
+/**
+ * A single unit of source code (e.g. a file, the contents of a textarea or a network command)
+ */
+open class SourceUnit(val identifier: String)
