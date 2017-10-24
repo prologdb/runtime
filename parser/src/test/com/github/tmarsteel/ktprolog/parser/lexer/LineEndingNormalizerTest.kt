37a4b504c1b9c14d1456b5a1cc041529119bfdc0
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-10-24 16:14:34 +0200

    Lexer: added LineEndingNormalizer

---
+++ parser/src/test/com/github/tmarsteel/ktprolog/parser/lexer/LineEndingNormalizerTest.kt
@@ -0,0 +1,44 @@
+package com.github.tmarsteel.ktprolog.parser.lexer
+
+import io.kotlintest.specs.FreeSpec
+
+class LineEndingNormalizerTest : FreeSpec(){init {
+    val source = "a\nabc\nab\rabcd\r\nhht\r"
+
+    "test" {
+        val normalizer = LineEndingNormalizer(source.asIterable().iterator())
+
+        normalizer.next() shouldEqual 'a'
+        normalizer.next() shouldEqual '\n'
+        normalizer.next() shouldEqual 'a'
+        normalizer.next() shouldEqual 'b'
+        normalizer.next() shouldEqual 'c'
+        normalizer.next() shouldEqual '\n'
+        normalizer.next() shouldEqual 'a'
+        normalizer.next() shouldEqual 'b'
+        normalizer.next() shouldEqual '\n'
+        normalizer.next() shouldEqual 'a'
+        normalizer.next() shouldEqual 'b'
+        normalizer.next() shouldEqual 'c'
+        normalizer.next() shouldEqual 'd'
+        normalizer.next() shouldEqual '\n'
+        normalizer.next() shouldEqual 'h'
+        normalizer.next() shouldEqual 'h'
+        normalizer.next() shouldEqual 't'
+        normalizer.next() shouldEqual '\n'
+
+        normalizer.hasNext() shouldEqual false
+        shouldThrow<NoSuchElementException> {
+            normalizer.next()
+        }
+    }
+
+    "empty" {
+        val normalizer = LineEndingNormalizer("".asIterable().iterator())
+
+        normalizer.hasNext() shouldEqual false
+        shouldThrow<NoSuchElementException> {
+            normalizer.next()
+        }
+    }
+}}
