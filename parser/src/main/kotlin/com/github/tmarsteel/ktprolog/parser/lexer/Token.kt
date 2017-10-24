5b5dd045401a92e5d1656e6dfaaa516cb0739034
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-10-24 16:30:07 +0200

    Parser: Added Token OO model

---
+++ parser/src/main/kotlin/com/github/tmarsteel/ktprolog/parser/lexer/Token.kt
@@ -0,0 +1,23 @@
+package com.github.tmarsteel.ktprolog.parser.lexer
+
+import com.github.tmarsteel.ktprolog.parser.source.SourceLocationRange
+
+enum class TokenType {
+    IDENTIFIER,
+    OPERATOR,
+    NUMERIC_LITERAL
+}
+
+enum class Operator(val text: String) {
+    PARANT_OPEN("("),
+    PARANT_CLOSE(")"),
+    FULL_STOP("."),
+    SEMICOLON(";"),
+    COMMA(",")
+}
+
+sealed class Token(val type: TokenType, val location: SourceLocationRange)
+class IdentifierToken(val textContent: String, location: SourceLocationRange) : Token(TokenType.IDENTIFIER, location)
+class OperatorToken(val operator: Operator, location: SourceLocationRange): Token(TokenType.OPERATOR, location)
+class NumericLiteralToken(val number: Number, location: SourceLocationRange): Token(TokenType.NUMERIC_LITERAL, location)
+
