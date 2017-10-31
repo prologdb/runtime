6c72f845f2c10b6be53438f19495d387af39a8dc
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-10-31 15:56:38 +0100

    Core: added OrQuery

---
+++ core/src/main/kotlin/com/github/tmarsteel/ktprolog/query/OrQuery.kt
@@ -0,0 +1,31 @@
+package com.github.tmarsteel.ktprolog.query
+
+import com.github.tmarsteel.ktprolog.RandomVariableScope
+import com.github.tmarsteel.ktprolog.VariableMapping
+import com.github.tmarsteel.ktprolog.knowledge.KnowledgeBase
+import com.github.tmarsteel.ktprolog.unification.Unification
+import com.github.tmarsteel.ktprolog.unification.VariableBucket
+import mapToArray
+import kotlin.coroutines.experimental.buildSequence
+
+class OrQuery(val goals: Array<out Query>) : Query {
+    override fun findProofWithin(kb: KnowledgeBase, initialVariables: VariableBucket, randomVarsScope: RandomVariableScope): Sequence<Unification> {
+        return buildSequence {
+            for (goal in goals) {
+                yieldAll(goal.findProofWithin(kb, initialVariables, randomVarsScope))
+            }
+        }
+    }
+
+    override fun withRandomVariables(randomVarsScope: RandomVariableScope, mapping: VariableMapping): Query {
+        return AndQuery(
+            goals.mapToArray { it.withRandomVariables(randomVarsScope, mapping) }
+        )
+    }
+
+    override fun substituteVariables(variableValues: VariableBucket): Query {
+        return AndQuery(
+            goals.mapToArray { it.substituteVariables(variableValues) }
+        )
+    }
+}
