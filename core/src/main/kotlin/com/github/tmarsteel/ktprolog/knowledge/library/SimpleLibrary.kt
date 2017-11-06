0fff48bd015a3a75ab9081c3a923fbf24e4c5f37
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-11-06 18:15:43 +0100

    Core: added default value for argument givenEntries of SimpleLibrary.construct

--- core/src/main/kotlin/com/github/tmarsteel/ktprolog/knowledge/library/SimpleLibrary.kt
+++ core/src/main/kotlin/com/github/tmarsteel/ktprolog/knowledge/library/SimpleLibrary.kt
@@ -5,7 +5,7 @@ package com.github.tmarsteel.ktprolog.knowledge.library
  * based on a plain [MutableList] and uses the default implementations
  * declared in [Library] and [MutableLibrary]
  */
-class SimpleLibrary(givenEntries: Iterable<LibraryEntry>) : MutableLibrary {
+class SimpleLibrary(givenEntries: Iterable<LibraryEntry> = emptyList()) : MutableLibrary {
     private val entries = ArrayList(givenEntries.toList())
 
     override val exports = entries
