fd3935df337d7703c1e12327251ec82ff8d12379
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-11-07 20:47:05 +0100

    Core: moved ArityMap out of DoublyIndexedLibrary for use in other code

---
+++ core/src/main/kotlin/com/github/tmarsteel/ktprolog/ArityMap.kt
@@ -0,0 +1,68 @@
+package com.github.tmarsteel.ktprolog
+
+import kotlin.coroutines.experimental.buildSequence
+
+/**
+ * A simplified [Map]<Integer,T>.
+ *
+ * The arity of prolog predicates is expected to be in the lower positive range. Because of that,
+ * reserving O(n) memory for the index where n is the number of different arities in the map will
+ * be efficient. As a result, a [Array]<T> is used for its performance advantages.
+ */
+open class ArityMap<T>(private var items: Array<T?> = Array<Any?>(6, {null}) as Array<T?>) {
+
+    private val itemsResizeMutex = Any()
+
+    /**
+     * The maximum arity this map can store without allocating additional memory
+     */
+    var capacity: Int
+        get() = items.size - 1
+        set(value) {
+            synchronized(itemsResizeMutex) {
+                items = Array<Any?>(value + 1, { index -> if (index < items.size) items[index] else null }) as Array<T?>
+            }
+        }
+
+    val arities: Iterable<Int> = buildSequence {
+        (0 until items.size)
+            .filter { items[it] != null }
+            .forEach { yield(it) }
+    }.asIterable()
+
+    operator fun get(arity: Int): T? {
+        if (arity < 0) {
+            throw IllegalArgumentException("The arity must positive or 0.")
+        }
+
+        if (arity >= items.size) {
+            return null
+        }
+
+        return items[arity]
+    }
+
+    operator fun set(arity: Int, item: T) {
+        if (arity < 0) {
+            throw IllegalArgumentException("The arity must positive or 0.")
+        }
+
+        if (arity >= items.size) {
+            capacity = arity
+        }
+
+        items[arity] = item
+    }
+
+    operator fun contains(arity: Int): Boolean {
+        if (arity < 0) {
+            throw IllegalArgumentException("The arity must positive or 0.")
+        }
+
+        if (arity >= items.size) {
+            return false
+        }
+
+        return items[arity] != null
+    }
+}
--- core/src/main/kotlin/com/github/tmarsteel/ktprolog/knowledge/library/DoublyIndexedLibrary.kt
+++ core/src/main/kotlin/com/github/tmarsteel/ktprolog/knowledge/library/DoublyIndexedLibrary.kt
@@ -1,7 +1,7 @@
 package com.github.tmarsteel.ktprolog.knowledge.library
 
+import com.github.tmarsteel.ktprolog.ArityMap
 import com.github.tmarsteel.ktprolog.term.Predicate
-import kotlin.coroutines.experimental.buildSequence
 
 /**
  * Indexes entries by arity and name in maps.
@@ -12,7 +12,7 @@ class DoublyIndexedLibrary : MutableLibrary {
      * The index. The key in the outer map corresponds to [LibraryEntry.name] and the
      * key of the inner map corresponds to [LibraryEntry.arity].
      */
-    private val index = mutableMapOf<String,ArityMap>()
+    private val index = mutableMapOf<String, ArityMapToLibraryEntries>()
 
     override fun findFor(predicate: Predicate): Iterable<LibraryEntry> {
         val arityMap = index[predicate.name] ?: return emptyList()
@@ -20,7 +20,7 @@ class DoublyIndexedLibrary : MutableLibrary {
     }
 
     override fun add(entry: LibraryEntry) {
-        val arityIndex: ArityMap
+        val arityIndex: ArityMapToLibraryEntries
 
         if (entry.name !in index) {
             arityIndex = ArityMap()
@@ -79,76 +79,17 @@ class DoublyIndexedLibrary : MutableLibrary {
 
             return cachedExports!!
         }
-
-    /**
-     * A simplified [Map]<Integer,T>.
-     *
-     * The arity of prolog predicates is expected to be in the lower positive range. Because of that,
-     * reserving O(n) memory for the index where n is the number of different arities in the map will
-     * be efficient. As a result, a [Array]<T> is used for its performance advantages.
-     */
-    class ArityMap {
-
-        private val itemsResizeMutex = Any()
-
-        private var items = Array<MutableList<LibraryEntry>?>(6, {null})
-
-        val arities: Iterable<Int> = buildSequence {
-            (0 until items.size)
-                .filter { items[it] != null }
-                .forEach { yield(it) }
-        }.asIterable()
-
-        operator fun get(arity: Int): MutableList<LibraryEntry>? {
-            if (arity < 0) {
-                throw IllegalArgumentException("The arity must positive or 0.")
-            }
-
-            if (arity >= items.size) {
-                return null
-            }
-
-            return items[arity]!!
-        }
-
-        operator fun set(arity: Int, item: MutableList<LibraryEntry>) {
-            if (arity < 0) {
-                throw IllegalArgumentException("The arity must positive or 0.")
-            }
-
-            if (arity >= items.size) {
-                synchronized(itemsResizeMutex) {
-                    items = Array(arity + 1, { index -> if (index < items.size) items[index] else null })
-                }
-            }
-
-            items[arity] = item
-        }
-
-        operator fun contains(arity: Int): Boolean {
-            if (arity < 0) {
-                throw IllegalArgumentException("The arity must positive or 0.")
-            }
-
-            if (arity >= items.size) {
-                return false
-            }
-
-            return items[arity] != null
-        }
-
-        /**
-         * Creates a reference copy of this map. Changes to the copy do not
-         * reflect onto this map; changes on the [LibraryEntry]s within **do**.
-         */
-        fun copy(): ArityMap {
-            val copy = ArityMap()
-            copy.items = Array(this.items.size, {
-                val listCopy = mutableListOf<LibraryEntry>()
-                listCopy.addAll(this.items[it]!!)
-                listCopy
-            })
-            return copy
-        }
-    }
 }
+
+private typealias ArityMapToLibraryEntries = ArityMap<MutableList<LibraryEntry>>
+
+/**
+ * Creates a reference copy of this map. Changes to the copy do not
+ * reflect onto this map; changes on the [LibraryEntry]s within **do**.
+ */
+private fun ArityMapToLibraryEntries.copy() =
+    ArityMap<MutableList<LibraryEntry>>(kotlin.Array(capacity + 1, {
+        val listCopy = kotlin.collections.mutableListOf<LibraryEntry>()
+        listCopy.addAll(this[it]!!)
+        listCopy
+    }))
