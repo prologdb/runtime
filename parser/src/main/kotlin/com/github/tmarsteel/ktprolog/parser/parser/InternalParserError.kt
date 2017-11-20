3c2c1d6a015df1dfa9d6c2097371da8e534c0b75
Tobias Marstaller <tobias.marstaller@gmail.com>
2017-11-20 13:19:54 +0100

    Parser: added feedback note to internal parser error

--- parser/src/main/kotlin/com/github/tmarsteel/ktprolog/parser/parser/InternalParserError.kt
+++ parser/src/main/kotlin/com/github/tmarsteel/ktprolog/parser/parser/InternalParserError.kt
@@ -1,3 +1,16 @@
 package com.github.tmarsteel.ktprolog.parser.parser
 
-class InternalParserError(message: String) : Exception(message)
+private const val FEEDBACK_NOTE = "Please send the stacktrace and steps to reproduce to the author of this library."
+
+class InternalParserError private constructor(actualMessage: Any) : Exception(actualMessage.toString()) {
+
+    // the (Any) constructor serves a purpose:
+    // Calling InternalParserError() should result in ex.message == FEEDBACK_NOTE
+    // Calling InternalParserError(someMessage) should result in ex.message == someMessage + " " + FEEDBACK_NOTE
+    // The any constructor is used to that (String) can be declared separately; otherwise the signatures would be
+    // equal and the code would not compile.
+
+
+    constructor() : this(FEEDBACK_NOTE as Any)
+    constructor(message: String) : this((message + " " + FEEDBACK_NOTE) as Any)
+}
