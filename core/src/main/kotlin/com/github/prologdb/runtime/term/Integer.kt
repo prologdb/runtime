30e65b8057d76e797d7ba182bdfacfb2a950d042
Tobias Marstaller <tobias.marstaller@hubject.com>
2018-05-29 16:42:48 +0200

    Added Integer term caching for integers in range [0; 65535] to optimize string performance

--- core/src/main/kotlin/com/github/prologdb/runtime/term/Integer.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/term/Integer.kt
@@ -5,54 +5,54 @@ import com.github.prologdb.runtime.RandomVariableScope
 import com.github.prologdb.runtime.unification.Unification
 import kotlin.math.pow
 
-open class Integer(val value: Long) : Number {
+open class Integer private constructor(val value: Long) : Number {
 
     override fun plus(other: Number) =
         when(other) {
-            is Integer -> Integer(this.value + other.value)
+            is Integer -> Integer.createUsingStringOptimizerCache(this.value + other.value)
             is Decimal -> Decimal(this.value.toDouble() + other.value)
             else -> throw PrologRuntimeException("Unsupported type of number")
         }
 
     override fun minus(other: Number) =
         when(other) {
-            is Integer -> Integer(this.value - other.value)
+            is Integer -> Integer.createUsingStringOptimizerCache(this.value - other.value)
             is Decimal -> Decimal(this.value.toDouble() - other.value)
             else -> throw PrologRuntimeException("Unsupported type of number")
         }
 
     override fun times(other: Number) =
         when(other) {
-            is Integer -> Integer(this.value * other.value)
+            is Integer -> Integer.createUsingStringOptimizerCache(this.value * other.value)
             is Decimal -> Decimal(this.value.toDouble() * other.value)
             else -> throw PrologRuntimeException("Unsupported type of number")
         }
 
     override fun div(other: Number) =
         when(other) {
-            is Integer -> Integer(this.value / other.value)
+            is Integer -> Integer.createUsingStringOptimizerCache(this.value / other.value)
             is Decimal -> Decimal(this.value.toDouble() / other.value)
             else -> throw PrologRuntimeException("Unsupported type of number")
         }
 
     override fun rem(other: Number) =
         when(other) {
-            is Integer -> Integer(this.value % other.value)
+            is Integer -> Integer.createUsingStringOptimizerCache(this.value % other.value)
             is Decimal -> Decimal(this.value.toDouble() % other.value)
             else -> throw PrologRuntimeException("Unsupported type of number")
         }
 
     override fun toThe(other: Number): Number {
         return when(other) {
-            is Integer -> Integer(this.value.toDouble().pow(other.value.toDouble()).toLong())
+            is Integer -> Integer.createUsingStringOptimizerCache(this.value.toDouble().pow(other.value.toDouble()).toLong())
             is Decimal -> Decimal(this.value.toDouble().pow(other.value))
             else -> throw PrologRuntimeException("Unsupported type of number")
         }
     }
 
-    override fun unaryPlus(): Number = Integer(+this.value)
+    override fun unaryPlus(): Number = Integer.createUsingStringOptimizerCache(+this.value)
 
-    override fun unaryMinus(): Number = Integer(-this.value)
+    override fun unaryMinus(): Number = Integer.createUsingStringOptimizerCache(-this.value)
 
     override fun compareTo(other: Number) =
         when(other) {
@@ -91,4 +91,23 @@ open class Integer(val value: Long) : Number {
     }
 
     override fun toString() = value.toString()
+
+    companion object {
+        /* The integers 0 through 65536 are cached: these are used for
+           strings and this avoids a lot of memory overhead. Also, this
+           likely applies to other uses-cases where small numbers are used.
+         */
+        private val cache: MutableMap<Short, Integer> = mutableMapOf()
+
+        operator fun invoke(value: Long): Integer = createUsingStringOptimizerCache(value)
+
+        fun createUsingStringOptimizerCache(value: Long): Integer {
+            if (value > Short.MAX_VALUE || value < Short.MIN_VALUE) {
+                return Integer(value) // constructor invocation
+            } else {
+                val valueAsShort = value.toShort()
+                return cache[valueAsShort] ?: { val r = Integer(value); cache[valueAsShort] = r; r }()
+            }
+        }
+    }
 }
