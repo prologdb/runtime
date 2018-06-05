69902a4eb1a67c258437ae4ea6d7c6b1c24b5538
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-06-05 08:39:45 +0200

    Added first prolog test cases for string_chars/2
    Still failing for a reason i dont know yet (after 30min of debugging)

---
+++ tests/src/test/prolog/string_chars.test.pl
@@ -0,0 +1,17 @@
+test "string_chars/2 conversion from string to list" by [
+    string_chars("foobar", X),
+    X = [f, o, o, b, a, r]
+].
+
+test "string_chars/2 conversion from list to string" by [
+    string_chars(X, [f, o, o, b, a, r]),
+    X = "foobar"
+].
+
+test "string_chars/2 with both instantiated: equal" by [
+    string_chars("foobar", [f, o, o, b, a, r])
+].
+
+test "string_chars/2 with both instantiated: not equal" by [
+    not(string_chars("foo", [b, a, r]))
+].
