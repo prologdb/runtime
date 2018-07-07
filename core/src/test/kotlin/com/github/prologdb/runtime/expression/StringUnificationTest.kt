b8ce12c87273cb07f39eb0655056c4248283f183
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-07-07 20:44:38 +0200

    Amends f3a3827

--- core/src/test/kotlin/com/github/prologdb/runtime/expression/StringUnificationTest.kt
+++ core/src/test/kotlin/com/github/prologdb/runtime/expression/StringUnificationTest.kt
@@ -29,18 +29,15 @@ class StringUnificationTest : FreeSpec() { init {
             val H = Variable("H")
             val T = Variable("T")
             PrologString("foobar") shouldUnifyWith PrologList(listOf(H), T) suchThat {
-                itHasExactlyOneSolution()
-                itHasASolutionSuchThat("H = 102, T = [111,111,98,97,114]") {
-                    it.variableValues[H] == PrologInteger(102)
-                    &&
-                    it.variableValues[T] == PrologList(listOf(
-                        PrologInteger(111),
-                        PrologInteger(111),
-                        PrologInteger(98),
-                        PrologInteger(97),
-                        PrologInteger(114)
-                    ))
-                }
+                it.variableValues[H] == PrologInteger(102)
+                &&
+                it.variableValues[T] == PrologList(listOf(
+                    PrologInteger(111),
+                    PrologInteger(111),
+                    PrologInteger(98),
+                    PrologInteger(97),
+                    PrologInteger(114)
+                ))
             }
         }
 
@@ -50,21 +47,18 @@ class StringUnificationTest : FreeSpec() { init {
             val H3 = Variable("H3")
             val T = Variable("T")
             PrologString("foobar") shouldUnifyWith PrologList(listOf(H1, H2, H3), T) suchThat {
-                itHasExactlyOneSolution()
-                itHasASolutionSuchThat("H1 = 102, H2 = 111, H3 = 111, T = [98,97,114]") {
-                    it.variableValues[H1] == PrologInteger(102)
-                    &&
-                    it.variableValues[H2] == PrologInteger(111)
-                    &&
-                    it.variableValues[H3] == PrologInteger(111)
-                    &&
-                    it.variableValues[T] == PrologList(listOf(
+                it.variableValues[H1] == PrologInteger(102)
+                &&
+                it.variableValues[H2] == PrologInteger(111)
+                &&
+                it.variableValues[H3] == PrologInteger(111)
+                &&
+                it.variableValues[T] == PrologList(listOf(
 
-                        PrologInteger(98),
-                        PrologInteger(97),
-                        PrologInteger(114)
-                    ))
-                }
+                    PrologInteger(98),
+                    PrologInteger(97),
+                    PrologInteger(114)
+                ))
             }
         }
     }
