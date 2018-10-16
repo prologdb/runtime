47d43f30033851718a7daef3d983c6f7521fc922
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-10-16 17:04:40 +0200

    Async: WIP implementing workable coroutines

---
+++ async/src/test/kotlin/com/github/prologdb/async/WorkableFutureTest.kt
@@ -0,0 +1,193 @@
+package com.github.prologdb.async
+
+import com.sun.xml.internal.ws.util.CompletedFuture
+import io.kotlintest.matchers.beGreaterThanOrEqualTo
+import io.kotlintest.matchers.should
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.matchers.shouldThrow
+import io.kotlintest.specs.FreeSpec
+import java.util.concurrent.CompletableFuture
+import kotlin.concurrent.thread
+
+class WorkableFutureTest : FreeSpec({
+    "immediate return" {
+        val hasRun = CompletableFuture<Unit>()
+
+        val future = WorkableFutureImpl {
+            hasRun.complete(Unit)
+            "foobar"
+        }
+
+        hasRun.isDone shouldBe false
+        future.step()
+        hasRun.isDone shouldBe true
+
+        future.isDone shouldBe true
+        future.get() shouldBe "foobar"
+    }
+
+    "immediate throw" {
+        val hasRun = CompletableFuture<Unit>()
+
+        val future = WorkableFutureImpl {
+            hasRun.complete(Unit)
+            throw Exception("Faiiilleeeed!!!")
+        }
+
+        hasRun.isDone shouldBe false
+        future.step()
+        hasRun.isDone shouldBe true
+
+        shouldThrow<Exception> {
+            future.get()
+        }
+    }
+
+    "wait for another future step-by-step successfully" {
+        val hasStarted = CompletableFuture<Unit>()
+        val returnedAfterWait = CompletableFuture<Unit>()
+        val waitingOn = CompletableFuture<String>()
+
+        val future = WorkableFutureImpl {
+            hasStarted.complete(Unit)
+
+            val result = await(waitingOn)
+            returnedAfterWait.complete(Unit)
+
+            result
+        }
+
+        // initial state
+        future.isDone shouldBe false
+        hasStarted.isDone shouldBe false
+        returnedAfterWait.isDone shouldBe false
+
+        // test that await(T) suspends
+        future.step()
+        future.isDone shouldBe false
+        hasStarted.isDone shouldBe true
+        returnedAfterWait.isDone shouldBe false
+
+        // test that step() does not resume the coroutine
+        // unless its completed
+        future.step()
+        future.isDone shouldBe false
+        returnedAfterWait.isDone shouldBe false
+
+        // test that await() correctly forwards the result
+        // of the awaited future
+        val value = "Hello World!!"
+        waitingOn.complete(value)
+
+        // unless step() is called again, the future should
+        // not move ahead
+        future.isDone shouldBe false
+        returnedAfterWait.isDone shouldBe false
+
+        future.step()
+
+        // completion
+        future.isDone shouldBe true
+        returnedAfterWait.isDone shouldBe true
+        future.get() shouldBe value
+    }
+
+    "wait for another future step-by-step exceptionally" {
+        val hasStarted = CompletableFuture<Unit>()
+        val waitingOn = CompletableFuture<String>()
+
+        val future = WorkableFutureImpl<Any> {
+            hasStarted.complete(Unit)
+
+            try {
+                await(waitingOn)
+                Unit
+            }
+            catch (ex: Throwable) {
+                ex
+            }
+        }
+
+        future.step()
+        future.isDone shouldBe false
+        hasStarted.isDone shouldBe true
+
+        future.step()
+        future.isDone shouldBe false
+        waitingOn.isCompletedExceptionally shouldBe false
+
+        val error = Exception("faiiil!")
+        waitingOn.completeExceptionally(error)
+
+        future.isDone shouldBe false
+        future.isCancelled shouldBe false
+        future.step()
+
+        future.isDone shouldBe true
+        future.isCancelled shouldBe false
+
+        future.get() shouldBe error
+    }
+
+    "await exception should not complete the future" {
+        val waitingOn = CompletableFuture<Unit>()
+        val caught = CompletableFuture<Throwable>()
+
+        val future = WorkableFutureImpl {
+            try {
+                await(waitingOn)
+            }
+            catch (ex: Throwable) {
+                caught.complete(ex)
+            }
+
+            "All fine"
+        }
+
+        val error = Exception("faaaaaaiiiilllllllll")
+
+        future.step()
+        waitingOn.completeExceptionally(error)
+        future.step()
+
+        caught.isDone shouldBe true
+        caught.get() shouldBe error
+
+        future.get() shouldBe "All fine"
+    }
+
+    "await completed does not suspend" {
+        val waitingOn = CompletedFuture(Unit, null)
+
+        val future = WorkableFutureImpl {
+            await(waitingOn)
+            "Done"
+        }
+
+        future.step()
+        future.isDone shouldBe true
+        future.get() shouldBe "Done"
+    }
+
+    "get waits on await future" {
+        val waitingOn = CompletableFuture<Unit>()
+
+        val future = WorkableFutureImpl {
+            await(waitingOn)
+            "Done"
+        }
+
+        val earliestStart = System.currentTimeMillis()
+        thread {
+            Thread.sleep(200)
+            waitingOn.complete(Unit)
+        }
+
+        future.get() shouldBe "Done"
+        val completedAt = System.currentTimeMillis()
+
+        (completedAt - earliestStart) should beGreaterThanOrEqualTo(200L)
+    }
+
+    // TODO: test cancel
+})
