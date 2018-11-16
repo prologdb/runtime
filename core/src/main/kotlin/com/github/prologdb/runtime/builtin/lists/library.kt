f2f718aef4d60905e775864684f69126997144b4
Tobias Marstaller <tobias.marstaller@gmail.com>
2018-11-16 16:48:17 +0100

    core: ListsLibrary is now public

--- core/src/main/kotlin/com/github/prologdb/runtime/builtin/lists/library.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/builtin/lists/library.kt
@@ -3,11 +3,8 @@ package com.github.prologdb.runtime.builtin.lists
 import com.github.prologdb.runtime.builtin.NativeLibraryBuilder
 import com.github.prologdb.runtime.builtin.nativeLibrary
 import com.github.prologdb.runtime.knowledge.library.Clause
-import com.github.prologdb.runtime.knowledge.library.DefaultOperatorRegistry
-import com.github.prologdb.runtime.knowledge.library.DoublyIndexedClauseStore
-import com.github.prologdb.runtime.knowledge.library.Library
 
-internal val ListsLibrary = nativeLibrary("lists") {
+val ListsLibrary = nativeLibrary("lists") {
     add(AppendBuiltin)
     add(MemberBuiltin)
     add(LengthBuiltin)
