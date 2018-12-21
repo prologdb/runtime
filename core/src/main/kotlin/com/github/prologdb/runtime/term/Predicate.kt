397ea1cfc51c57485d6eb03cab227db88e023b0f
Tobias Marstaller <tobias.marstaller@hubject.com>
2018-12-21 01:46:00 +0100

    WIP implementing operator-aware toString on Predicate (infix is missing)

--- core/src/main/kotlin/com/github/prologdb/runtime/term/Predicate.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/term/Predicate.kt
@@ -4,6 +4,10 @@ import com.github.prologdb.async.LazySequenceBuilder
 import com.github.prologdb.runtime.RandomVariableScope
 import com.github.prologdb.runtime.knowledge.ProofSearchContext
 import com.github.prologdb.runtime.knowledge.library.Clause
+import com.github.prologdb.runtime.knowledge.library.OperatorDefinition
+import com.github.prologdb.runtime.knowledge.library.OperatorRegistry
+import com.github.prologdb.runtime.knowledge.library.OperatorType
+import com.github.prologdb.runtime.knowledge.library.OperatorType.*
 import com.github.prologdb.runtime.unification.Unification
 import com.github.prologdb.runtime.unification.VariableBucket
 import sensibleHashCode
@@ -113,6 +117,10 @@ open class Predicate(
         return name + "(" + arguments.joinToString(", ") + ")"
     }
 
+    override fun toStringUsingOperatorNotations(operators: OperatorRegistry): String {
+        return toStringUsingOperatorNotationsInternal(operators).first
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Predicate) return false
@@ -132,4 +140,134 @@ open class Predicate(
 
 class PredicateBuilder(private val predicateName: String) {
     operator fun invoke(vararg arguments: Term) = Predicate(predicateName, arguments)
+}
+
+/**
+ * @return first: the result string, second: the precedence of the term, third: the topmost/outmost operator
+ *         in the string (or null if none)
+ */
+private fun Term.toStringUsingOperatorNotationsInternal(operators: OperatorRegistry): Triple<String, Short, OperatorDefinition?> {
+    if (this !is Predicate) {
+        return Triple(this.toStringUsingOperatorNotations(operators), 0, null)
+    }
+    
+    val operator = operators.getOperatorDefinitionsFor(name)
+        .firstOrNull { it.type.arity == this.arity }
+        ?: return toStringThisUsingStrictNotationArgumentsUsingOperatorNotations(operators)
+    
+    when {
+        operator.type.isPrefix -> {
+            val arg = arguments[0]
+            val argumentTriple = arg.toStringUsingOperatorNotationsInternal(operators)
+            
+            val parenthesisRequirement = if (operator.precedence > argumentTriple.second) {
+                // priorities are clear, looking at associativity or putting parenthesis not necessary
+                ParenthesisRequirement.NOT_REQUIRED
+            }
+            else if (operator.precedence < argumentTriple.second) {
+                // priorities are directly inverted to what would be if we simply prepended the operator
+                // thus, parenthesis are required.
+                ParenthesisRequirement.REQUIRED
+            }
+            else {
+                // priorities are ambiguous, need to look at associativity
+                
+                if (operator.type == FY && argumentTriple.third?.type in setOf(XF, XFX, XFY)) {
+                    // associativity removes the ambiguity in this case
+                    ParenthesisRequirement.NOT_REQUIRED
+                } else {
+                    // priorities are directly inverted to what would be if we simply prepended the operator
+                    // thus, parenthesis are required.
+                    ParenthesisRequirement.REQUIRED
+                }
+            }
+            
+            return when (parenthesisRequirement) {
+                ParenthesisRequirement.NOT_REQUIRED -> Triple(operator.name + " " + argumentTriple.first, operator.precedence, operator)
+                ParenthesisRequirement.REQUIRED -> {
+                    // for prefix, parenthesis are, to the reader, equal to invocation syntax (compare op(a) op (a)).
+                    // for easier reading and parsing, we go with the invocation syntax (no whitespace)
+                    return Triple(
+                        operator.name + "(" + argumentTriple.first + ")",
+                        operator.precedence,
+                        operator
+                    )
+                }
+            }
+        }
+        
+        operator.type.isPostfix -> {
+            val arg = arguments[0]
+            val argumentTriple = arg.toStringUsingOperatorNotationsInternal(operators)
+
+            val parenthesisRequirement = if (operator.precedence > argumentTriple.second) {
+                // priorities are clear, looking at associativity or putting parenthesis not necessary
+                ParenthesisRequirement.NOT_REQUIRED
+            }
+            else if (operator.precedence < argumentTriple.second) {
+                // priorities are directly inverted to what would be if we simply prepended the operator
+                // thus, parenthesis are required.
+                ParenthesisRequirement.REQUIRED
+            }
+            else {
+                // priorities are ambiguous, need to look at associativity
+
+                if (argumentTriple.third?.type in setOf(FX, XFX, YFX) && operator.type == YF) {
+                    // associativity removes the ambiguity in this case
+                    ParenthesisRequirement.NOT_REQUIRED
+                } else {
+                    // priorities are directly inverted to what would be if we simply prepended the operator
+                    // thus, parenthesis are required.
+                    ParenthesisRequirement.REQUIRED
+                }
+            }
+
+            return when (parenthesisRequirement) {
+                ParenthesisRequirement.NOT_REQUIRED -> Triple(argumentTriple.first + " " + operator.name, operator.precedence, operator)
+                ParenthesisRequirement.REQUIRED -> {
+                    return Triple(
+                        "(" + argumentTriple.first + ") " + operator.name,
+                        operator.precedence,
+                        operator
+                    )
+                }
+            }
+        }
+        
+        operator.type.isInfix -> {
+            // TODO, this is make-the-test-green-TDD-style-code
+            return Triple(
+                arguments[0].toStringUsingOperatorNotationsInternal(operators).first + " " + operator.name + " " + arguments[1].toStringUsingOperatorNotationsInternal(operators).first,
+                operator.precedence,
+                operator
+            )
+        }
+        
+        else -> throw RuntimeException("This should not have happened. ${OperatorType::class.qualifiedName}.isPrefix / .isPostfix / .isInfix are erroneous (exactly one of the three MUST be true)")
+    }
+}
+
+private enum class ParenthesisRequirement {
+    REQUIRED,
+    NOT_REQUIRED
+}
+
+/**
+ * @return compatible with [toStringUsingOperatorNotationsInternal], but second always 0 and third always null.
+ */
+private fun Predicate.toStringThisUsingStrictNotationArgumentsUsingOperatorNotations(operators: OperatorRegistry): Triple<String, Short, OperatorDefinition?> {
+    val argumentStrings = arguments.map {
+        val triple = it.toStringUsingOperatorNotationsInternal(operators)
+        if (triple.third?.name == ",") "(${triple.first})" else triple.first
+    }
+
+    return Triple(
+        argumentStrings.joinToString(
+            separator = ", ",
+            prefix = "$name(", // TODO name that needs escaping
+            postfix = ")"
+        ),
+        0,
+        null
+    )
 }
---
+++ core/src/test/kotlin/com/github/prologdb/runtime/term/OperatorNotationToStringTest.kt
@@ -0,0 +1,397 @@
+package com.github.prologdb.runtime.term
+
+import com.github.prologdb.runtime.knowledge.library.DefaultOperatorRegistry
+import com.github.prologdb.runtime.knowledge.library.OperatorDefinition
+import com.github.prologdb.runtime.knowledge.library.OperatorType
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.FreeSpec
+
+class OperatorNotationToStringTest : FreeSpec({
+    "prefix operator nesting" - {
+        val a = Atom("a")
+        val b = Atom("b")
+        val prefix = PredicateBuilder("prefix")
+        val postfix = PredicateBuilder("postfix")
+        val infix = PredicateBuilder("infix")
+        
+        "prefix has higher priority" - {
+            "nesting XF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                
+                val term = prefix(postfix(a))
+                
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a postfix"
+            }
+    
+            "nesting YF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YF, "postfix"))
+    
+                val term = prefix(postfix(a))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a postfix"
+            }
+    
+            "nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a infix b"
+            }
+    
+            "nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a infix b"
+            }
+    
+            "nesting YFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a infix b"
+            }
+        }
+        
+        "prefix has lower priority" - {
+            "nesting XF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(100, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+    
+                val term = prefix(postfix(a))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a postfix)"
+            }
+    
+            "nesting YF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(100, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YF, "postfix"))
+    
+                val term = prefix(postfix(a))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a postfix)"
+            }
+    
+            "nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(100, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a infix b)"
+            }
+    
+            "nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(100, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a infix b)"
+            }
+    
+            "nesting YFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(100, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a infix b)"
+            }
+        }
+        
+        "equal priority" - {
+            "FX nesting XF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+    
+                val term = prefix(postfix(a))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a postfix)"
+            }
+    
+            "FX nesting YF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YF, "postfix"))
+    
+                val term = prefix(postfix(a))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a postfix)"
+            }
+    
+            "FX nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a infix b)"
+            }
+    
+            "FX nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a infix b)"
+            }
+    
+            "FX nesting YFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix(a infix b)"
+            }
+            
+            "FY nesting XF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FY, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+    
+                val term = prefix(postfix(a))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a postfix"
+            }
+    
+            "FY nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FY, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a infix b"
+            }
+    
+            "FY nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FY, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+    
+                val term = prefix(infix(a, b))
+    
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a infix b"
+            }
+        }
+    }
+
+    "postfix operator nesting" - {
+        val a = Atom("a")
+        val b = Atom("b")
+        val prefix = PredicateBuilder("prefix")
+        val postfix = PredicateBuilder("postfix")
+        val infix = PredicateBuilder("infix")
+
+        "postfix has higher priority" - {
+            "nesting FX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a postfix"
+            }
+
+            "nesting FY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FY, "prefix"))
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a postfix"
+            }
+
+            "nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "a infix b postfix"
+            }
+
+            "nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "a infix b postfix"
+            }
+
+            "nesting YFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "a infix b postfix"
+            }
+        }
+
+        "postfix has lower priority" - {
+            "nesting FX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(100, OperatorType.XF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(prefix a) postfix"
+            }
+
+            "nesting FY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FY, "prefix"))
+                registry.defineOperator(OperatorDefinition(100, OperatorType.XF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(prefix a) postfix"
+            }
+
+            "nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+
+            "nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(300, OperatorType.XFY, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+
+            "nesting YFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(300, OperatorType.YFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+        }
+
+        "equal priority" - {
+            "XF nesting FX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(prefix a) postfix"
+            }
+
+            "XF nesting FY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FY, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(prefix a) postfix"
+            }
+
+            "XF nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+
+            "XF nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+
+            "XF nesting YFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+
+            "YF nesting XF" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.FX, "prefix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YF, "postfix"))
+
+                val term = postfix(prefix(a))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "prefix a postfix"
+            }
+
+            "YF nesting XFX" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFX, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "a infix b postfix"
+            }
+
+            "YF nesting XFY" {
+                val registry = DefaultOperatorRegistry()
+                registry.defineOperator(OperatorDefinition(200, OperatorType.YF, "postfix"))
+                registry.defineOperator(OperatorDefinition(200, OperatorType.XFY, "infix"))
+
+                val term = postfix(infix(a, b))
+
+                term.toStringUsingOperatorNotations(registry) shouldBe "(a infix b) postfix"
+            }
+        }
+    }
+})
