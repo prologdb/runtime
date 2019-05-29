11e2103994e0309612d9fbdac30b21c261aa6544
Tobias Marstaller <tobias.marstaller@gmail.com>
2019-05-29 12:15:55 +0200

    Added basic implementation of compile_predicates/1
    Will not change much, but doesn't do anything either. Optimization and compilation yet to be implemented.

--- core/src/main/kotlin/com/github/prologdb/runtime/builtin/dynamic/compile_predicates__1.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/builtin/dynamic/compile_predicates__1.kt
@@ -1,11 +1,20 @@
 package com.github.prologdb.runtime.builtin.dynamic
 
 import com.github.prologdb.runtime.ClauseIndicator
+import com.github.prologdb.runtime.FullyQualifiedClauseIndicator
 import com.github.prologdb.runtime.PrologRuntimeException
-import com.github.prologdb.runtime.builtin.nativePredicate
+import com.github.prologdb.runtime.builtin.nativeRule
+import com.github.prologdb.runtime.optimization.TailcallOptimizer
+import com.github.prologdb.runtime.proofsearch.ASTPrologPredicate
+import com.github.prologdb.runtime.proofsearch.DelegatableCallable
+import com.github.prologdb.runtime.proofsearch.DynamicPrologPredicate
+import com.github.prologdb.runtime.proofsearch.PrologCallable
 import com.github.prologdb.runtime.term.PrologList
+import com.github.prologdb.runtime.unification.Unification
 
-internal val BuiltinCompilePredicates = nativePredicate("compile_predicates", 1) { args, context ->
+private val tailcallOptimizer = TailcallOptimizer()
+
+internal val BuiltinCompilePredicates = nativeRule("compile_predicates", 1) { args, context ->
     val arg0 = args[0]
     if (arg0 !is PrologList) {
         throw PrologRuntimeException("Argument 1 to compile_predicates/1 must be a list, ${arg0.prologTypeName} given")
@@ -15,9 +24,33 @@ internal val BuiltinCompilePredicates = nativePredicate("compile_predicates", 1)
         throw PrologRuntimeException("Argument 1 to compile_predicates/1 must not have a tail")
     }
 
-    val indicators = arg0.elements.mapIndexed { index, term ->
-        ClauseIndicator.ofIdiomatic(term, "element ${index + 1} in argument 1 to compile_predicates/1")
+    val fqIndicatorAndCallables: Sequence<Pair<FullyQualifiedClauseIndicator, PrologCallable>> = arg0.elements
+        .asSequence()
+        .mapIndexed { index, term ->
+            val simpleIndicator = ClauseIndicator.ofIdiomatic(term, "element ${index + 1} in argument 1 to compile_predicates/1")
+            context.resolveCallable(simpleIndicator)
+                ?: throw PrologRuntimeException("Predicate $simpleIndicator is not defined")
+        }
+
+    fqIndicatorAndCallables.forEach {
+        if (it.second !is DelegatableCallable) {
+            throw PrologRuntimeException("Cannot optimize and/or compile ${it.first}: not an instance of ${DelegatableCallable::class.java.name}")
+        }
     }
 
-    TODO()
+    fqIndicatorAndCallables.forEach { (_, callable) ->
+        if (callable is DynamicPrologPredicate) {
+            callable.seal()
+        }
+    }
+
+    for ((fqIndicator, callable) in fqIndicatorAndCallables) {
+        if (callable is ASTPrologPredicate) {
+            tailcallOptimizer.tryOptimize(callable)?.let { optimized ->
+                callable.setDelegate(optimized, true)
+            }
+        }
+    }
+
+    yield(Unification.TRUE)
 }
