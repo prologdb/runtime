4d38a9ead241806c4f0498f9a3f4ff35809f99d1
Tobias Marstaller <tobias.marstaller@gmail.com>
2019-06-14 17:50:39 +0200

    Added basic lambda support to call/1

--- core/src/main/kotlin/com/github/prologdb/runtime/builtin/dynamic/call__1.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/builtin/dynamic/call__1.kt
@@ -17,19 +17,23 @@ import com.github.prologdb.runtime.unification.VariableBucket
 internal val BuiltinCall1 = object : Rule(CompoundTerm("call", arrayOf(Variable("_Arg1"))), PredicateInvocationQuery(CompoundTerm("__nativeCode", emptyArray()))) {
     override val fulfill: suspend LazySequenceBuilder<Unification>.(Array<out Term>, ProofSearchContext) -> Unit = { args, ctxt ->
         val goalInput = args[0]
-
-        val goal = when (goalInput) {
-            is CompoundTerm -> goalInput
-            is Atom -> CompoundTerm(goalInput.name, emptyArray())
-            else -> throw PrologRuntimeException("Argument 1 given to call/1 must be an atom or a compound term, got ${goalInput.prologTypeName}")
-        }
-
         val sourceInfo = (goalInput as? HasPrologSource)?.sourceInformation ?: NullSourceInformation
 
-        ctxt.fulfillAttach(
-            this,
-            PredicateInvocationQuery(goal, sourceInfo),
-            VariableBucket()
-        )
+        val goalInputAsLambda = goalInput.tryCastToLambda()
+        if (goalInputAsLambda != null) {
+            goalInputAsLambda.fulfill(this, emptyArray(), ctxt)
+        } else {
+            val goal = when (goalInput) {
+                is CompoundTerm -> goalInput
+                is Atom -> CompoundTerm(goalInput.name, emptyArray())
+                else -> throw PrologRuntimeException("Argument 1 given to call/1 must be an atom or a compound term, got ${goalInput.prologTypeName}")
+            }
+
+            ctxt.fulfillAttach(
+                this,
+                PredicateInvocationQuery(goal, sourceInfo),
+                VariableBucket()
+            )
+        }
     }
 }
--- core/src/main/kotlin/com/github/prologdb/runtime/builtin/dynamic/module.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/builtin/dynamic/module.kt
@@ -1,11 +1,16 @@
 package com.github.prologdb.runtime.builtin.dynamic
 
+import com.github.prologdb.runtime.HasPrologSource
+import com.github.prologdb.runtime.builtin.getInvocationStackFrame
 import com.github.prologdb.runtime.builtin.nativeModule
+import com.github.prologdb.runtime.builtin.prologSourceInformation
+import com.github.prologdb.runtime.proofsearch.Rule
 import com.github.prologdb.runtime.query.AndQuery
 import com.github.prologdb.runtime.query.OrQuery
 import com.github.prologdb.runtime.query.PredicateInvocationQuery
 import com.github.prologdb.runtime.query.Query
 import com.github.prologdb.runtime.term.CompoundTerm
+import com.github.prologdb.runtime.term.Term
 
 val DynamicsModule = nativeModule("dynamics") {
     add(BuiltinFindAll)
@@ -20,14 +25,17 @@ val DynamicsModule = nativeModule("dynamics") {
  * queries).
  */
 fun compoundToQuery(compoundTerm: CompoundTerm): Query {
+    val sourceInformation = (compoundTerm as? HasPrologSource)?.sourceInformation
+        ?: getInvocationStackFrame().prologSourceInformation
+
     if (compoundTerm.arity != 2) {
-        return PredicateInvocationQuery(compoundTerm)
+        return PredicateInvocationQuery(compoundTerm, sourceInformation)
     }
 
     if (compoundTerm.functor == "," || compoundTerm.functor == ";") {
         val allArgumentsCompound = compoundTerm.arguments.all { it is CompoundTerm }
         if (!allArgumentsCompound) {
-            return PredicateInvocationQuery(compoundTerm)
+            return PredicateInvocationQuery(compoundTerm, sourceInformation)
         }
 
         val argumentsConverted = compoundTerm.arguments.map { compoundToQuery(it as CompoundTerm) }.toTypedArray()
@@ -38,5 +46,22 @@ fun compoundToQuery(compoundTerm: CompoundTerm): Query {
         }
     }
     // else:
-    return PredicateInvocationQuery(compoundTerm)
+    return PredicateInvocationQuery(compoundTerm, sourceInformation)
+}
+
+fun Term.tryCastToLambda(): Rule? {
+    if (this !is CompoundTerm || this.functor != ":-" || this.arity != 2) {
+        return null
+    }
+
+    val head = this.arguments[0] as? CompoundTerm ?: return null
+    val query = (this.arguments[1] as? CompoundTerm ?: return null)?.let {
+        try {
+            compoundToQuery(it)
+        } catch (ex: IllegalArgumentException) {
+            return null
+        }
+    }
+
+    return Rule(head, query)
 }
