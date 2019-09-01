4e6e1bf93107518809c15018c8f85b11cb2ca6af
Tobias Marstaller <tobias.marstaller@gmail.com>
2019-09-01 16:07:17 +0200

    Core: added float/1 and compound/1 typecheck builtins

--- core/src/main/kotlin/com/github/prologdb/runtime/builtin/typesafety.kt
+++ core/src/main/kotlin/com/github/prologdb/runtime/builtin/typesafety.kt
@@ -3,7 +3,9 @@ package com.github.prologdb.runtime.builtin
 import com.github.prologdb.runtime.PrologRuntimeException
 import com.github.prologdb.runtime.proofsearch.Rule
 import com.github.prologdb.runtime.term.Atom
+import com.github.prologdb.runtime.term.CompoundTerm
 import com.github.prologdb.runtime.term.PrologDecimal
+import com.github.prologdb.runtime.term.PrologDictionary
 import com.github.prologdb.runtime.term.PrologInteger
 import com.github.prologdb.runtime.term.PrologList
 import com.github.prologdb.runtime.term.PrologNumber
@@ -36,12 +38,23 @@ val TypeofBuiltin = nativeRule("typeof", 2) { args, ctxt ->
     behavesSemiDeterministic()
 }
 
+val CompoundBuiltin = nativeRule("compound", 1) { args, ctxt ->
+    val arg0 = args[0]
+
+    if (arg0 is CompoundTerm || arg0 is PrologList || arg0 is PrologDictionary) {
+        yield(Unification.TRUE)
+    }
+}.apply {
+    behavesSemiDeterministic()
+}
+
 val TypeSafetyModule = nativeModule("typesafety") {
     // all of these are /1 tests
     add(typeCheckBuiltin<Atom>("atom"))
 
     add(typeCheckBuiltin<PrologInteger>("integer"))
     add(typeCheckBuiltin<PrologDecimal>("decimal"))
+    add(typeCheckBuiltin<PrologDecimal>("float"))
     add(typeCheckBuiltin<PrologNumber>("number"))
 
     add(typeCheckBuiltin<PrologString>("string"))
@@ -55,6 +68,7 @@ val TypeSafetyModule = nativeModule("typesafety") {
     add(testingBuiltin("nonground") { it.variables.isNotEmpty() })
 
     add(TypeofBuiltin)
+    add(CompoundBuiltin)
 }
 
 private inline fun <reified T : Term> typeCheckBuiltin(name: String, negated: Boolean = false)  = typeCheckBuiltin(name, T::class.java, negated)
