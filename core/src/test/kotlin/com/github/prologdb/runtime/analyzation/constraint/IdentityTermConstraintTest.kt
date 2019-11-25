da7c7ab341a5ce07a983c9e0af32780b4b90a544
Tobias Marstaller <tobias.marstaller@gmail.com>
2019-11-25 19:40:27 +0100

    Added IdentityTermConstraintTest

---
+++ core/src/test/kotlin/com/github/prologdb/runtime/analyzation/constraint/IdentityTermConstraintTest.kt
@@ -0,0 +1,80 @@
+package com.github.prologdb.runtime.analyzation.constraint
+
+import com.github.prologdb.runtime.term.Atom
+import com.github.prologdb.runtime.term.PrologDecimal
+import com.github.prologdb.runtime.term.PrologInteger
+import com.github.prologdb.runtime.term.PrologNumber
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.FreeSpec
+
+class IdentityTermConstraintTest : FreeSpec() { init {
+    "check" - {
+        "with same type" {
+            IdentityTermConstraint(PrologInteger(4)).check(PrologInteger(3)) shouldBe false
+        }
+
+        "with same term instance" {
+            val term = Atom("bla")
+            IdentityTermConstraint(term).check(term) shouldBe true
+        }
+
+        "with equal terms of different identity" {
+            val termA = Atom("foo")
+            val termB = Atom("foo")
+            check(termA == termB)
+            check(termA !== termB)
+            IdentityTermConstraint(termA).check(termB) shouldBe true
+        }
+    }
+
+    "and" - {
+        "noop" {
+            val constraint = IdentityTermConstraint(PrologInteger(1))
+            constraint.and(NoopConstraint) shouldBe constraint
+        }
+
+        "impossible" {
+            IdentityTermConstraint(Atom("hans")).and(ImpossibleConstraint) shouldBe ImpossibleConstraint
+        }
+
+        "type" - {
+            "the type constraints onto the same type" {
+                val constraint = IdentityTermConstraint(Atom("hans"))
+                constraint.and(TypeTermConstraint(Atom::class.java)) shouldBe constraint
+            }
+
+            "the type constraints on a supertype" {
+                val constraint = IdentityTermConstraint(PrologInteger(123123))
+                constraint.and(TypeTermConstraint(PrologNumber::class.java)) shouldBe constraint
+            }
+
+            "the type constraints on a disjoint type" {
+                val constraint = IdentityTermConstraint(PrologDecimal(51024.0))
+                constraint.and(TypeTermConstraint(PrologInteger::class.java)) shouldBe ImpossibleConstraint
+            }
+        }
+
+        "identity" - {
+            "same instance" {
+                val term = Atom("foo")
+                val constraint = IdentityTermConstraint(term)
+                constraint.and(IdentityTermConstraint(term)) shouldBe constraint
+            }
+
+            "identical terms of different identity" {
+                val termA = Atom("bla")
+                val termB = Atom("bla")
+                check(termA == termB)
+                check(termA !== termB)
+
+                val constraint = IdentityTermConstraint(termA)
+                constraint.and(IdentityTermConstraint(termB)) shouldBe constraint
+            }
+
+            "unequal terms" {
+                IdentityTermConstraint(PrologInteger(512312)).and(IdentityTermConstraint(PrologInteger(63251))) shouldBe ImpossibleConstraint
+            }
+        }
+    }
+}
+}
