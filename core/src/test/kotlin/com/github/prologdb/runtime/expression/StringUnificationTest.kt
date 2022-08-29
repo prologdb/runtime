478babac857a2606af047a112eaecab97d96b503
Tobias Marstaller <tobias.marstaller@gmail.com>
2022-08-29 23:03:23 +0200

    #14: follow up with changes

--- core/src/test/kotlin/com/github/prologdb/runtime/expression/StringUnificationTest.kt
+++ core/src/test/kotlin/com/github/prologdb/runtime/expression/StringUnificationTest.kt
@@ -3,8 +3,8 @@ package com.github.prologdb.runtime.expression
 import com.github.prologdb.runtime.shouldNotUnifyWith
 import com.github.prologdb.runtime.shouldUnifyWith
 import com.github.prologdb.runtime.suchThat
-import com.github.prologdb.runtime.term.PrologInteger
 import com.github.prologdb.runtime.term.PrologList
+import com.github.prologdb.runtime.term.PrologNumber
 import com.github.prologdb.runtime.term.PrologString
 import com.github.prologdb.runtime.term.Variable
 import io.kotest.core.spec.style.FreeSpec
@@ -29,14 +29,14 @@ class StringUnificationTest : FreeSpec() { init {
             val H = Variable("H")
             val T = Variable("T")
             PrologString("foobar") shouldUnifyWith PrologList(listOf(H), T) suchThat {
-                it.variableValues[H] == PrologInteger(102)
+                it.variableValues[H] == PrologNumber(102)
                 &&
                 it.variableValues[T] == PrologList(listOf(
-                    PrologInteger(111),
-                    PrologInteger(111),
-                    PrologInteger(98),
-                    PrologInteger(97),
-                    PrologInteger(114)
+                    PrologNumber(111),
+                    PrologNumber(111),
+                    PrologNumber(98),
+                    PrologNumber(97),
+                    PrologNumber(114)
                 ))
             }
         }
@@ -47,17 +47,16 @@ class StringUnificationTest : FreeSpec() { init {
             val H3 = Variable("H3")
             val T = Variable("T")
             PrologString("foobar") shouldUnifyWith PrologList(listOf(H1, H2, H3), T) suchThat {
-                it.variableValues[H1] == PrologInteger(102)
+                it.variableValues[H1] == PrologNumber(102)
                 &&
-                it.variableValues[H2] == PrologInteger(111)
+                it.variableValues[H2] == PrologNumber(111)
                 &&
-                it.variableValues[H3] == PrologInteger(111)
+                it.variableValues[H3] == PrologNumber(111)
                 &&
                 it.variableValues[T] == PrologList(listOf(
-
-                    PrologInteger(98),
-                    PrologInteger(97),
-                    PrologInteger(114)
+                    PrologNumber(98),
+                    PrologNumber(97),
+                    PrologNumber(114)
                 ))
             }
         }
